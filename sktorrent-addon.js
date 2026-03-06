32cd945b69f83e9a5b62bd26f73de1e12a94fcaf
zdenci22 <zdenci22@gmail.com>
2026-03-06 19:10:35 +0100

    Update SKTorrent addon to version 1.0.4

--- sktorrent-addon.js
+++ sktorrent-addon.js
@@ -14,8 +14,8 @@ const SEARCH_URL = `${BASE_URL}/torrent/torrents_v2.php`;
 
 const builder = addonBuilder({
     id: "org.stremio.sktorrent",
-    version: "1.0.0",
-    name: "SKTorrent",
+    version: "1.0.4",
+    name: "SKtorCZ",
     description: "Streamuj torrenty z SKTorrent.eu (filmy aj seriály)",
     types: ["movie", "series"],
     catalogs: [
@@ -213,6 +213,7 @@ builder.defineCatalogHandler(({ type, id }) => {
 console.log("\ud83d\udccc Manifest debug výpis:", builder.getInterface().manifest);
 serveHTTP(builder.getInterface(), { port: 7000 });
 console.log("\ud83d\ude80 SKTorrent addon beží na http://localhost:7000/manifest.json");
+
 
 
 
